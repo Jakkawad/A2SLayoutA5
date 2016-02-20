2cec243d52588279d3849a2933d8e85ee287306b
Jakkawad <jakkawadchaiplee@Jakkawads-MacBook-Pro.local>
2016-02-20 21:38:07 +0700

    A2SLayoutA5
    A2SLayoutA5 with out coding

---
+++ A2SLayoutA5/Menu2TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Menu2TableViewCell.swift
+//  A2SLayoutA5
+//
+//  Created by Jakkawad Chaiplee on 2/20/2559 BE.
+//  Copyright © 2559 Jakkawad Chaiplee. All rights reserved.
+//
+
+import UIKit
+
+class Menu2TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-A2SLayoutA5/Pods-A2SLayoutA5-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_A2SLayoutA5VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_A2SLayoutA5VersionString[];
+
